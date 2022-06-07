e721f321d6586add4dc64a68e1733ce6e2d03d7b
Justin Lu <justin@airplane.dev>
2022-06-06 17:22:10 -0700

    export

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { appendOutput, setOutput } from "./output";
 import { execute } from "./tasks";
 export { ExecuteOptions } from "./tasks";
+export { executeTask } from "./activities";
 
 export default {
   appendOutput,
