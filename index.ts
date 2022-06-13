c761078377fd11d1824f9579ea4502b013926348
Ameya Khare <ameya@airplane.dev>
2022-06-13 18:12:17 -0400

    Export from index

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 export { registerActivities } from "./activities";
+import email from "./email";
+import mongodb from "./mongodb";
 import { appendOutput, setOutput } from "./output";
+import rest from "./rest";
+import slack from "./slack";
 import sql from "./sql";
 import { execute } from "./tasks";
 export { ExecuteOptions } from "./tasks";
@@ -8,5 +12,9 @@ export default {
   appendOutput,
   setOutput,
   execute,
+  email,
+  mongodb,
+  rest,
+  slack,
   sql,
 };
