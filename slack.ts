01253cd98f7b8bf0caba90923c5dcdd146173ab4
Ameya Khare <ameya@airplane.dev>
2022-06-14 19:27:21 -0400

    Add slack virtual resource id

--- slack.ts
+++ slack.ts
@@ -2,7 +2,6 @@ import { Run, ExecuteOptions, executeInternal } from "./tasks";
 
 export default {
   message: async <Output = undefined | null>(
-    slackResourceID: string,
     channelName: string,
     message: string,
     opts?: ExecuteOptions
@@ -10,7 +9,7 @@ export default {
     return executeInternal(
       "airplane:slack_message",
       { channelName, message },
-      { slack: slackResourceID },
+      { slack: "res00000000zteamslack" },
       opts
     );
   },
