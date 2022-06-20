f1fcd3560b1929fff271d6232af00c324c8e3767
Colin King <colin@airplane.dev>
2022-06-20 09:46:48 -0400

    cleanup index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+// Methods
 export { appendOutput, setOutput } from "./internal/output";
 export { execute } from "./internal/execute";
+
+// Built-ins
 export * from "./internal/builtins";
-export { ClientOptions } from "./internal/api/client";
+
+// API
+export type { ClientOptions } from "./internal/api/client";
+export type { RunStatus, Run } from "./internal/api/types";
